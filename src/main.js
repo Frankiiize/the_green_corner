4ca5e96417c8c6a1983e48cd8b3dd8cbcd9db7df
frank <frankiiize@gmail.com>
2026-09-14 23:11:18 -0300

    swiper

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import { register } from "swiper/element/bundle";
 import "./scss/main.scss";
 
 import "./components/button/index";
 import "./components/header/index";
 import "./components/card/index";
+
+// import function to register Swiper custom elements
+// register Swiper custom elements
+register();
 
 console.log("load");
